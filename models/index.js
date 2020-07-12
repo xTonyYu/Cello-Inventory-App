584074fb638a439b1d87f71de1f9c084bef2aea8
Lucian nicolescu <lucian.nicolescu.cello@gmail.com>
2020-07-12 15:00:50 -0700

    made the models index file

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const MONGODB_URI = 'mongodb://localhost:27017/celloShop';
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+})
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.log(`MongoDB connection error: ${err}`));
+
+module.exports = {
+  Cello: require('./cellos'),
+  Accesories: require('./accessories'),
+  Makers: require('./makers')
+};
