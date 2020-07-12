630c8bb1f55bdf4fbd553aaaca4f2a3f40d23a5a
Lucian nicolescu <lucian.nicolescu.cello@gmail.com>
2020-07-12 14:44:36 -0700

    accesories schema done

--- models/accessories.js
+++ models/accessories.js
@@ -1,1 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const accesorieSchema =new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    brand: {
+        type: String,
+    },
+    year: {
+        type: Number,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    cost: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    descrption: {
+        type: String,
+    },
+    forInstrument: {
+        type: String,
+    }
+}, {timestamps: true});
+
+const Accesories = mongoose.model('Accesories', accesorieSchema);
+
+module.exports = Accesories;
