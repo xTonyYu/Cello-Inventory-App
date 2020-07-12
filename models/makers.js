848ed39a81a13ed04cc37d414807a0dd3c68465c
Lucian nicolescu <lucian.nicolescu.cello@gmail.com>
2020-07-12 14:50:52 -0700

    Maker schema done

--- models/makers.js
+++ models/makers.js
@@ -1,1 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { model } = require('./accessories');
+
+const makerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    bio: {
+        type: String,
+    },
+    photo: {
+        type: String,
+    }
+}, {timestamps: true});
+
+const Maker = mongoose.model('Maker', makerSchema);
+
+module.exports = Maker;
